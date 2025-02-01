7803c1d45531c8092d3410ffe0598a0a36ae2cef
Justinianus2001 <lengochoang681@gmail.com>
2025-02-01 21:35:58 +0700

    The exit builtin

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
-import sys
-
-
 def main():
-    sys.stdout.write("$ ")
-
     # Wait for user input
-    command = input()
+    command = input("$ ")
+    if command == "exit 0":
+        return 0
     print(f"{command}: command not found")
     main()
 
