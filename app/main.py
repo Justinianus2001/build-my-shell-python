a034af48c2b651c381ee12a025a85dd6b8cbd73e
Justinianus2001 <lengochoang681@gmail.com>
2025-02-01 22:51:08 +0700

    The type builtin: executable files

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+import os
+
+
 def main():
     builtins = {"exit", "echo", "type"}
+    paths = os.getenv("PATH").split(":")
 
     # Wait for user input
     command = input("$ ")
@@ -10,8 +14,12 @@ def main():
         case ["echo", *text]:
             print(*text)
         case ["type", builtin]:
+            command_path = next((f"{path}/{builtin}" for path in paths if os.path.exists(f"{path}/{builtin}")), None)
+
             if builtin in builtins:
                 print(f"{builtin} is a shell builtin")
+            elif command_path:
+                print(f"{builtin} is {command_path}")
             else:
                 print(f"{builtin}: not found")
         case _:
