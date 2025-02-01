497910ddc89bece6cefa0b956c690d8ea7564e16
Justinianus2001 <lengochoang681@gmail.com>
2025-02-01 22:20:48 +0700

    The type builtin: builtins

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
 def main():
+    builtins = {"exit", "echo", "type"}
+
     # Wait for user input
     command = input("$ ")
 
-    command_split = command.split()
-
-    match command_split[0]:
-        case "exit":
-            return 0
-        case "echo":
-            print(" ".join(command_split[1:]))
+    match command.split():
+        case ["exit", code]:
+            return int(code)
+        case ["echo", *text]:
+            print(*text)
+        case ["type", builtin]:
+            if builtin in builtins:
+                print(f"{builtin} is a shell builtin")
+            else:
+                print(f"{builtin}: not found")
         case _:
             print(f"{command}: command not found")
 
