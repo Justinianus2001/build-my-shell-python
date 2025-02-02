87cb3021bc16a37aef240dec30741cdbb12ae640
Justinianus2001 <lengochoang681@gmail.com>
2025-02-03 02:45:57 +0700

    Builtin completion

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 import functools
 import os
+import readline
 import shlex
 import subprocess
 import sys
+
+
+builtins = {"exit", "echo", "type", "pwd", "cd"}
+redirect_symbols = ["2>>", "1>>", ">>", "2>", "1>", ">"]
+
+
+def completer(text, state):
+    options = [command + " " for command in builtins if command.startswith(text)]
+    return options[state] if state < len(options) else None
 
 
 def handle_redirects(command, out, err, symbol):
@@ -31,56 +41,55 @@ def get_command_path(command):
 
 
 def main():
-    builtins = {"exit", "echo", "type", "pwd", "cd"}
-    redirect_symbols = ["2>>", "1>>", ">>", "2>", "1>", ">"]
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(completer)
 
-    # Wait for user input
-    command = input("$ ").strip()
-    out = sys.stdout
-    err = sys.stderr
+    while True:
+        # Wait for user input
+        command = input("$ ").strip()
+        out = sys.stdout
+        err = sys.stderr
 
-    for symbol in redirect_symbols:
-        if symbol in command:
-            command, out, err = handle_redirects(command, out, err, symbol)
-            break
+        for symbol in redirect_symbols:
+            if symbol in command:
+                command, out, err = handle_redirects(command, out, err, symbol)
+                break
 
-    print = functools.partial(__builtins__.print, file=out)
-    print_err = functools.partial(__builtins__.print, file=err)
+        print = functools.partial(__builtins__.print, file=out)
+        print_err = functools.partial(__builtins__.print, file=err)
 
-    match shlex.split(command):
-        case ["exit", code]:
-            return int(code)
-        case ["echo", *text]:
-            print(*text)
-        case ["type", builtin]:
-            command_path = get_command_path(builtin)
+        match shlex.split(command):
+            case ["exit", code]:
+                return int(code)
+            case ["echo", *text]:
+                print(*text)
+            case ["type", builtin]:
+                command_path = get_command_path(builtin)
 
-            if builtin in builtins:
-                print(f"{builtin} is a shell builtin")
-            elif command_path:
-                print(f"{builtin} is {command_path}")
-            else:
-                print(f"{builtin}: not found")
-        case ["pwd"]:
-            print(os.getcwd())
-        case ["cd", path]:
-            try:
-                os.chdir(os.path.expanduser(path))
-            except FileNotFoundError:
-                print_err(f"cd: {path}: No such file or directory")
-        case [file, *args]:
-            if get_command_path(file):
-                subprocess.run([file, *args], stdout=out, stderr=err)
-            else:
-                print_err(f"{command}: command not found")
+                if builtin in builtins:
+                    print(f"{builtin} is a shell builtin")
+                elif command_path:
+                    print(f"{builtin} is {command_path}")
+                else:
+                    print(f"{builtin}: not found")
+            case ["pwd"]:
+                print(os.getcwd())
+            case ["cd", path]:
+                try:
+                    os.chdir(os.path.expanduser(path))
+                except FileNotFoundError:
+                    print_err(f"cd: {path}: No such file or directory")
+            case [file, *args]:
+                if get_command_path(file):
+                    subprocess.run([file, *args], stdout=out, stderr=err)
+                else:
+                    print_err(f"{command}: command not found")
 
-    if out is not sys.stdout:
-        out.close()
+        if out is not sys.stdout:
+            out.close()
 
-    if err is not sys.stderr:
-        err.close()
-
-    main()
+        if err is not sys.stderr:
+            err.close()
 
 
 if __name__ == "__main__":
